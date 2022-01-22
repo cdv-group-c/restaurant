7d96b6cbf79921667f4b90e10faeb4193876ee83
sansiro <emilo01@o2.pl>
2022-01-22 20:45:10 +0100

    Add summary module

--- restaurant/summary.cpp
+++ restaurant/summary.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 
 using namespace std;
+void showUserAddress(string adress);
+void showTableNumber(int tableNumber)
+{
+	cout << "Twoj numer stolika to: " << tableNumber << endl;
+}
+
+
+
+
